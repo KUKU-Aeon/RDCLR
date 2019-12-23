6b50bea837fefe088dddf3482262a3268d13ca98
KUKU-Aeon <56968565+KUKU-Aeon@users.noreply.github.com>
2019-12-24 01:04:01 +0300

    upd
    asddas

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
         function resize() {
             width = window.innerWidth;
             if (block)
-                block.style.width = `98.8vw`;
+                block.style.width = `98.7vw`;
         }
 
         resize();
