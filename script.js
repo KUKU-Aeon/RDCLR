ec5e0ad5205cc8d30d721e330f84a1453092fd9c
KUKU-Aeon <56968565+KUKU-Aeon@users.noreply.github.com>
2019-12-24 09:47:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -239,7 +239,7 @@ document.addEventListener('DOMContentLoaded', function () {
     button.addEventListener('click', function() {
         if (action === false) {
             if (menu) menu.classList.add('active');
-            menu.style.transform = 'translateX(0)';
+            menu.style.left = '0';
             if (button) button.classList.add('active');
             action = true;
         } else {
