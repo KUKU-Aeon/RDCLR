ecdd4cee183dd30c87e99571fecceecfbbd61ea0
KUKU-Aeon <56968565+KUKU-Aeon@users.noreply.github.com>
2019-12-25 05:33:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
+
     let header = document.querySelector('header');
     let prevScroll = pageYOffset;
     let scroll = pageYOffset;
     let hidden = false;
+
+    let scrolledDown = 0;
 
     window.addEventListener('scroll', function () {
         if (!header)
@@ -10,22 +13,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
         scroll = pageYOffset;
 
-        console.log(scroll - prevScroll);
+        if ((header) && !hidden)
+            header.classList.remove('hidden');
 
         if ((scroll - prevScroll > 0) && !hidden) {
             header.classList.add('hidden');
             hidden = true;
-            prevScroll = scroll
-        }
+            prevScroll = scroll;
+            scrolledDown = 0;
+        } else {
+            if (hidden && (scroll - prevScroll < 0))
+                scrolledDown += scroll - prevScroll;
+            else
+                scrolledDown = 0;
 
-        if ((scroll - prevScroll <= -35) && hidden) {
+            if ((scrolledDown < -50) && hidden) {
+                header.classList.remove('hidden');
+                hidden = false;
+                prevScroll = scroll
+            }
+        }
+        if (hidden && pageYOffset === 0) {
             header.classList.remove('hidden');
             hidden = false;
             prevScroll = scroll
         }
 
         prevScroll = scroll
-    })
+    });
 });
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -50,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
         function resize() {
             width = window.innerWidth;
             if (block)
-                block.style.width = `98.6vw`;
+                block.style.width = '100%';
         }
 
         resize();
@@ -156,6 +171,14 @@ function createForm(form, mess) {
 
     function validate() {
         let result = true;
+        let el = form.querySelector('select');
+        if (el.selectedIndex === 0){
+            el.classList.add('error');
+            el.addEventListener('change', function () {
+                el.classList.remove('error');
+            });
+            result = false;
+        }
 
         for (let i = 0; elements.length > i; i++) {
             let el = elements[i];
@@ -184,8 +207,14 @@ function createForm(form, mess) {
             }
 
             if (el.tagName.toLocaleLowerCase() === 'select' && !el.getAttribute('multiply')){
+                console.log('el.selectedIndex');
                 if (el.selectedIndex === 0){
-                    el.value = "student"
+                    console.log(el.selectedIndex);
+                    el.classList.add('error');
+                    el.addEventListener('input', () => {
+                        el.classList.remove('error')
+                    });
+                    result = false;
                 }
             }
         }
@@ -208,6 +237,11 @@ function createForm(form, mess) {
             document.body.style.overflow = "hidden";
 
             let button = textmess.querySelector('button');
+            overlay.addEventListener('click', function () {
+                overlay.style.display = 'none';
+                document.body.style.overflow = "scroll";
+            })
+
             button.addEventListener('click', function () {
                 overlay.style.display = 'none';
                 document.body.style.overflow = "scroll";
@@ -339,4 +373,112 @@ document.addEventListener('DOMContentLoaded', function () {
             }
     });
 
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    let multiItemSlider = (function () {
+        return function (selector, config) {
+            let
+                _mainElement = document.querySelector(selector), // основный элемент блока
+                _sliderWrapper = _mainElement.querySelector('.slider__wrapper_home'), // обертка для .slider-item
+                _sliderItems = _mainElement.querySelectorAll('.slider__item'), // элементы (.slider-item)
+                _sliderControls = _mainElement.querySelectorAll('.slider__control'), // элементы управления
+                _sliderControlLeft = _mainElement.querySelector('.slider__control_left'), // кнопка "LEFT"
+                _sliderControlRight = _mainElement.querySelector('.slider__control_right'), // кнопка "RIGHT"
+                _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width), // ширина обёртки
+                _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width), // ширина одного элемента
+                _positionLeftItem = 0, // позиция левого активного элемента
+                _transform = 0, // значение транфсофрмации .slider_wrapper
+                _step = _itemWidth / _wrapperWidth * 100, // величина шага (для трансформации)
+                _items = []; // массив элементов
+
+            // наполнение массива _items
+            _sliderItems.forEach(function (item, index) {
+                _items.push({item: item, position: index, transform: 0});
+            });
+
+            let position = {
+                getItemMin: function () {
+                    let indexItem = 0;
+                    _items.forEach(function (item, index) {
+                        if (item.position < _items[indexItem].position) {
+                            indexItem = index;
+                        }
+                    });
+                    return indexItem;
+                },
+                getItemMax: function () {
+                    let indexItem = 0;
+                    _items.forEach(function (item, index) {
+                        if (item.position > _items[indexItem].position) {
+                            indexItem = index;
+                        }
+                    });
+                    return indexItem;
+                },
+                getMin: function () {
+                    return _items[position.getItemMin()].position;
+                },
+                getMax: function () {
+                    return _items[position.getItemMax()].position;
+                }
+            }
+
+            let _transformItem = function (direction) {
+                let nextItem;
+                if (direction === 'right') {
+                    _positionLeftItem++;
+                    if ((_positionLeftItem + _wrapperWidth / _itemWidth - 1) > position.getMax()) {
+                        nextItem = position.getItemMin();
+                        _items[nextItem].position = position.getMax() + 1;
+                        _items[nextItem].transform += _items.length * 100;
+                        _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
+                    }
+                    _transform -= _step;
+                }
+                if (direction === 'left') {
+                    _positionLeftItem--;
+                    if (_positionLeftItem < position.getMin()) {
+                        nextItem = position.getItemMax();
+                        _items[nextItem].position = position.getMin() - 1;
+                        _items[nextItem].transform -= _items.length * 100;
+                        _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
+                    }
+                    _transform += _step;
+                }
+                _sliderWrapper.style.transform = 'translateX(' + _transform + '%)';
+            }
+
+            // обработчик события click для кнопок "назад" и "вперед"
+            let _controlClick = function (e) {
+                if (e.target.classList.contains('slider__control')) {
+                    e.preventDefault();
+                    let direction = e.target.classList.contains('slider__control_right') ? 'right' : 'left';
+                    _transformItem(direction);
+                }
+            };
+
+            let _setUpListeners = function () {
+                // добавление к кнопкам "назад" и "вперед" обрботчика _controlClick для событя click
+                _sliderControls.forEach(function (item) {
+                    item.addEventListener('click', _controlClick);
+                });
+            }
+
+            // инициализация
+            _setUpListeners();
+
+            return {
+                right: function () { // метод right
+                    _transformItem('right');
+                },
+                left: function () { // метод left
+                    _transformItem('left');
+                }
+            }
+
+        }
+    }());
+
+    let slider = multiItemSlider('.slider_home')
 });
